532eb7694e3c89e84062a2012dade34d7ce0ae24
Christina Krawiec <ckrawiec@sas.upenn.edu>
2016-07-19 14:27:36 -0400

    tree function

--- cosmosLBGselect.py
+++ cosmosLBGselect.py
@@ -3,8 +3,65 @@ import time
 import esutil
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.spatial import ckdtree
 from astropy.io import ascii,fits
 from myutils import match
+
+def ntree(vals, errs, truevals):
+    knear = 1500
+    truetree = ckdtree.cKDTree(truevals)
+
+    out = []
+    for val, err in zip(vals, errs):
+        dnear, inear = truetree.query(val, k=knear)
+        truearr = truetree.data[inear]
+
+        covI = 1./err**2.
+        A = 1./np.sqrt( (2.*np.pi)**len(val)  * np.prod(err**2.) )
+
+        diff = val-truearr
+        B = -0.5 * np.sum(diff**2.*covI, axis=1)
+        C = A * np.exp(B)
+        
+        out.append(np.sum(C))
+
+    return np.array(out)
+
+
+def nwrapper(args):
+    return n(*args)
+
+def n(vals, errs, truevals):
+    """
+    sum the gaussian likelihoods L(vals|truevals) over truevals using the errs on vals
+    vals, errs, and truevals are lists or arrays of data/error vectors 
+    """
+    out = np.array([])
+
+    nchunks = 500
+    ntruechunks = 1000
+
+    chunks = itertools.izip([vals[i:i+nchunks] for i in xrange(0, len(vals), nchunks)], 
+                            [errs[i:i+nchunks] for i in xrange(0, len(vals), nchunks)])
+    
+    for chunk, errchunk in chunks:
+        trueout = np.zeros(len(chunk))
+    
+        covIs = 1./errchunk**2.
+        A = 1./np.sqrt( (2.*np.pi)**len(vals[0])  * np.prod(errchunk**2., axis=1 ) )
+
+        truechunks = (truevals[i:i+ntruechunks] for i in xrange(0, len(truevals), ntruechunks))
+        for truechunk in truechunks:
+            diff = chunk[:,np.newaxis,:]-truechunk[np.newaxis,:,:]
+
+            B = -0.5 * np.sum(diff**2.*covIs[:,np.newaxis], axis=2)
+            C = A[:,np.newaxis] * np.exp(B)
+            
+            trueout += np.sum(C, axis=1)
+
+        out = np.concatenate((out, trueout))
+    
+    return out
 
 
 def n1(vals, errs, truevals):
@@ -34,7 +91,7 @@ def n1(vals, errs, truevals):
             trueout += np.sum(C, axis=1)
 
         out = np.concatenate((out, trueout))
-        print len(out)
+
     return out
 
 def n2(vals, errs, truevals):
@@ -81,7 +138,7 @@ def n3(vals, errs, truevals):
 def main():
     
     #data sets
-    sva1_gold = fits.open('/Users/Christina/COSMOS/sva1_gold_detmodel_gals.fits')[1].data
+    sva1_gold = fits.open('/Users/Christina/DES/data/sva1_gold_detmodel_gals.fits')[1].data
 
     #COSMOS2015 photo-z
     #z_cosmos = 9.99 --> X-ray object, z_cosmos = 0 --> star
@@ -188,83 +245,58 @@ def main():
                                  hi_z_mag_cosmos['r'][:N_try_hi_cosmos],
                                  hi_z_mag_cosmos['i'][:N_try_hi_cosmos]) )
 
-    start1lo = time.time()
-    n1_lo_z = n1(lomags, lomagerrs, cosmoslomags)
-    print "For n1 (gold and cosmos chunks, no subtract): {}-d mag vector, {} gold galaxies, {} COSMOS lo-z galaxies: {}".format(len(lomags[0]),
-                                                                                                                                N_try_lo,
-                                                                                                                                N_try_lo_cosmos,
-                                                                                                                                time.time()-start1lo)
-    start2lo = time.time()
-    n2_lo_z = n2(lomags, lomagerrs, cosmoslomags)
-    print "For n2 (cosmos chunks, np.subtract): {}-d mag vector, {} gold galaxies, {} COSMOS lo-z galaxies: {}".format(len(lomags[0]),
-                                                                                                                       N_try_lo,
-                                                                                                                       N_try_lo_cosmos,
-                                                                                                                       time.time()-start2lo)
-    start3lo = time.time()
-    n3_lo_z = n3(lomags, lomagerrs, cosmoslomags)
-    print "For n3 (cosmos chunks, no subtract): {}-d mag vector, {} gold galaxies, {} COSMOS lo-z galaxies: {}".format(len(lomags[0]),
-                                                                                                                       N_try_lo,
-                                                                                                                       N_try_lo_cosmos,
-                                                                                                                       time.time()-start3lo)
-    start1hi = time.time()
-    n1_hi_z = n1(himags, himagerrs, cosmoshimags)
-    print "For n1: {}-d mag vector, {} gold galaxies, {} COSMOS hi-z galaxies: {}".format(len(himags[0]),
-                                                                                          N_try_hi,
-                                                                                          N_try_hi_cosmos,
-                                                                                          time.time()-start1hi)
-    start2hi = time.time()
-    n2_hi_z = n2(himags, himagerrs, cosmoshimags)
-    print "For n2: {}-d mag vector, {} gold galaxies, {} COSMOS hi-z galaxies: {}".format(len(himags[0]),
-                                                                                          N_try_hi,
-                                                                                          N_try_hi_cosmos,
-                                                                                          time.time()-start2hi)
-    start3hi = time.time()
-    n3_hi_z = n3(himags, himagerrs, cosmoshimags)
-    print "For n3: {}-d mag vector, {} gold galaxies, {} COSMOS hi-z galaxies: {}".format(len(himags[0]),
-                                                                                          N_try_hi,
-                                                                                          N_try_hi_cosmos,
-                                                                                          time.time()-start3hi)
 
-    print len(n1_hi_z), len(n2_hi_z), len(n3_hi_z), np.all(n1_lo_z==n2_lo_z), np.all(n2_lo_z==n3_lo_z), np.all(n1_hi_z==n2_hi_z)
+    def ntest(vals, errs, truevals, nfunc, N_try, N_try_cosmos):
+        start = time.time()
+        out = nfunc(vals, errs, truevals)
+        end = time.time()
+        print "For {}: {}-d mag vector, {} gold galaxies, {} COSMOS galaxies: {}".format(nfunc.func_name,
+                                                                                         len(vals[0]),
+                                                                                         N_try,
+                                                                                         N_try_cosmos,
+                                                                                         end-start)
+
+        return out
+
+    def plotn(n_lo, n_hi, save_file):
+        plt.scatter( mag_sva1_gold['r'][:N_try_hi] - mag_sva1_gold['i'][:N_try_hi],
+                     mag_sva1_gold['g'][:N_try_hi] - mag_sva1_gold['r'][:N_try_hi],
+                     c=(n_hi/N_try_hi_cosmos)/( (n_hi/N_try_hi_cosmos)+(n_lo/N_try_lo_cosmos) ),
+                     edgecolor='none')
+        plt.xlabel('mag_detmodel_r - mag_detmodel_i')
+        plt.ylabel('mag_detmodel_g - mag_detmodel_r')
+        plt.colorbar(label='hi/(hi+lo)')
+        plt.xlim(-1,4)
+        plt.ylim(-1,4)
+        plt.savefig(save_file)
+        plt.close()
+
+    n_lo_z = ntest(lomags, lomagerrs, cosmoslomags, n, N_try_lo, N_try_lo_cosmos)
+    n1_lo_z = ntest(lomags, lomagerrs, cosmoslomags, n1, N_try_lo, N_try_lo_cosmos)
+    n2_lo_z = ntest(lomags, lomagerrs, cosmoslomags, n2, N_try_lo, N_try_lo_cosmos)
+    n3_lo_z = ntest(lomags, lomagerrs, cosmoslomags, n3, N_try_lo, N_try_lo_cosmos)
+    ntree_lo_z = ntest(lomags, lomagerrs, cosmoslomags, ntree, N_try_lo, N_try_lo_cosmos)
+
+    n_hi_z = ntest(himags, himagerrs, cosmoshimags, n, N_try_hi, N_try_hi_cosmos)
+    n1_hi_z = ntest(himags, himagerrs, cosmoshimags, n1, N_try_hi, N_try_hi_cosmos)
+    n2_hi_z = ntest(himags, himagerrs, cosmoshimags, n2, N_try_hi, N_try_hi_cosmos)
+    n3_hi_z = ntest(himags, himagerrs, cosmoshimags, n3, N_try_hi, N_try_hi_cosmos)
+    ntree_hi_z = ntest(himags, himagerrs, cosmoshimags, ntree, N_try_hi, N_try_hi_cosmos)
+
+
+    plotn(n_lo_z, n_hi_z, 'hi_z_cosmos_mag_densities_ri_gr_n')
+    plotn(n1_lo_z, n1_hi_z, 'hi_z_cosmos_mag_densities_ri_gr_n1')
+    plotn(n2_lo_z, n2_hi_z, 'hi_z_cosmos_mag_densities_ri_gr_n2')
+    plotn(n3_lo_z, n3_hi_z, 'hi_z_cosmos_mag_densities_ri_gr_n3')
+    plotn(ntree_lo_z, ntree_hi_z, 'hi_z_cosmos_mag_densities_ri_gr_ntree')
+
+    if (np.all(n1_lo_z==n2_lo_z) and np.all(n2_lo_z==n3_lo_z) and np.all(n1_hi_z==n_hi_z)):
+        check = 'True'
+    else:
+        check = 'False'
+
+    print "Outputs are the same for n,n1,n2,n3?:" + check
     
-
-    plt.scatter( mag_sva1_gold['r'][:N_try_hi] - mag_sva1_gold['i'][:N_try_hi],
-                 mag_sva1_gold['g'][:N_try_hi] - mag_sva1_gold['r'][:N_try_hi],
-                 c=(n1_hi_z/N_try_hi_cosmos)/( (n1_hi_z/N_try_hi_cosmos)+(n1_lo_z/N_try_lo_cosmos) ),
-                 edgecolor='none')
-    plt.xlabel('mag_detmodel_r - mag_detmodel_i')
-    plt.ylabel('mag_detmodel_g - mag_detmodel_r')
-    plt.colorbar(label='hi/(hi+lo)')
-    plt.xlim(-1,4)
-    plt.ylim(-1,4)
-    plt.savefig('hi_z_cosmos_mag_densities_ri_gr_n1')
-    plt.close()
-
-    plt.scatter( mag_sva1_gold['r'][:N_try_hi] - mag_sva1_gold['i'][:N_try_hi],
-                 mag_sva1_gold['g'][:N_try_hi] - mag_sva1_gold['r'][:N_try_hi],
-                 c=(n2_hi_z/N_try_hi_cosmos)/( (n2_hi_z/N_try_hi_cosmos)+(n2_lo_z/N_try_lo_cosmos) ),
-                 edgecolor='none')
-    plt.xlabel('mag_detmodel_r - mag_detmodel_i')
-    plt.ylabel('mag_detmodel_g - mag_detmodel_r')
-    plt.colorbar(label='hi/(hi+lo)')
-    plt.xlim(-1,4)
-    plt.ylim(-1,4)
-    plt.savefig('hi_z_cosmos_mag_densities_ri_gr_n2')
-    plt.close()
-    
-
-    plt.scatter( mag_sva1_gold['r'][:N_try_hi] - mag_sva1_gold['i'][:N_try_hi],
-                 mag_sva1_gold['g'][:N_try_hi] - mag_sva1_gold['r'][:N_try_hi],
-                 c=(n3_hi_z/N_try_hi_cosmos)/( (n3_hi_z/N_try_hi_cosmos)+(n3_lo_z/N_try_lo_cosmos) ),
-                 edgecolor='none')
-    plt.xlabel('mag_detmodel_r - mag_detmodel_i')
-    plt.ylabel('mag_detmodel_g - mag_detmodel_r')
-    plt.colorbar(label='hi/(hi+lo)')
-    plt.xlim(-1,4)
-    plt.ylim(-1,4)
-    plt.savefig('hi_z_cosmos_mag_densities_ri_gr_n3')
-    plt.close()
-                          
 
     
 if __name__=="__main__":
