be78cb0e072c08f462dc365d2a7460f388fef9cf
Christina Krawiec <christina.i.krawiec@gmail.com>
2016-08-18 17:28:05 -0400

    machine flexible, fixed print

--- plot_probability.py
+++ plot_probability.py
@@ -1,12 +1,16 @@
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 from astropy.io import fits
 from myutils import match
 
+home_dir = '/home/ckrawiec/'
+
 def main():
-    sv = fits.open('/Users/Christina/DES/data/sva1_gold_detmodel_gals.fits')[1].data
-    hi = fits.open('/Users/Christina/DES/magnification/lbgselect/hi-z_mag_probability.fits')[1].data
-    lo = fits.open('/Users/Christina/DES/magnification/lbgselect/lo-z_mag_probability_test.fits')[1].data
+    sv = fits.open(home_dir+'DES/data/sva1_gold_detmodel_gals.fits')[1].data
+    hi = fits.open(home_dir+'DES/magnification/lbgselect/hi-z_mag_probability.fits')[1].data
+    lo = fits.open(home_dir+'DES/magnification/lbgselect/lo-z_mag_probability_test.fits')[1].data
     
     hi.sort(), sv.sort(), lo.sort()
     
@@ -18,14 +22,15 @@ def main():
     
     
     Phi = hi['hi-z_prob'][rid] / (hi['hi-z_prob'][rid] + lo['lo-z_prob'][rid])
-    Plo = lo['hi-z_prob'][rid] / (hi['hi-z_prob'][rid] + lo['lo-z_prob'][rid])
+    Plo = lo['lo-z_prob'][rid] / (hi['hi-z_prob'][rid] + lo['lo-z_prob'][rid])
     
-    print "P(hi-z)>=0.8: {}/{}".format(len(np.where(Phi>=0.8)), len(Phi))
+    print "P(hi-z)>=0.8: {}/{}".format(len(Phi[Phi>=0.8]), len(Phi))
                 
     plt.scatter(Plo, Phi, edgecolor='none')
+    plt.ylim(0.8, 1.0)
     plt.xlabel('P(lo-z)')
     plt.ylabel('P(hi-z)')
-    plt.savefig('Phi-vs-Plo')
+    plt.savefig(home_dir+'DES/magnification/lbgselect/Phi-vs-Plo')
     plt.close()
 
 if __name__=="__main__":
