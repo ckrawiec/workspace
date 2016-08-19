af601c4452e1f6f46ff8ad9b07361fa53012c595
Christina Krawiec <ckrawiec@sas.upenn.edu>
2016-08-19 14:55:02 -0400

    z-prob using cosmos w/noise

---
+++ cosmos+noise.py
@@ -0,0 +1,15 @@
+#a few different ways to add noise
+# - get noise from neighbors for a given ra/dec
+# - get limiting mag from map, use mag/err relation
+
+#open data catalog
+#healpixify errors (mean or median?)
+
+#open cosmos catalog
+#separate into groups
+#at each RA/DEC pixel
+# for each groupi
+#  for each groupj
+#   compute P(groupj|groupi+noise) 
+
+#save results to table
