163136a91581cde5376ef83e1f42fa3a76594481
Christina Krawiec <christina.i.krawiec@gmail.com>
2016-08-19 14:42:03 -0400

    compare old and new code

--- plot_probability.py
+++ plot_probability.py
@@ -10,27 +10,32 @@ home_dir = '/home/ckrawiec/'
 def main():
     sv = fits.open(home_dir+'DES/data/sva1_gold_detmodel_gals.fits')[1].data
     hi = fits.open(home_dir+'DES/magnification/lbgselect/hi-z_mag_probability.fits')[1].data
+    hi_test = fits.open(home_dir+'DES/magnification/lbgselect/hi-z_mag_probability_test.fits')[1].data
     lo = fits.open(home_dir+'DES/magnification/lbgselect/lo-z_mag_probability_test.fits')[1].data
     
-    hi.sort(), sv.sort(), lo.sort()
+    hi.sort(), sv.sort(), lo.sort(), hi_test.sort()
     
     svid, rid = match(sv['coadd_objects_id'], hi['coadd_objects_id'])
     
     ri = sv['mag_detmodel_r'][svid] - sv['mag_detmodel_i'][svid]
     gr = sv['mag_detmodel_g'][svid] - sv['mag_detmodel_r'][svid]
     
-    
-    
     Phi = hi['hi-z_prob'][rid] / (hi['hi-z_prob'][rid] + lo['lo-z_prob'][rid])
     Plo = lo['lo-z_prob'][rid] / (hi['hi-z_prob'][rid] + lo['lo-z_prob'][rid])
     
     print "P(hi-z)>=0.8: {}/{}".format(len(Phi[Phi>=0.8]), len(Phi))
+    print "P(hi-z)==P(hi-z)_test? {}".format(np.all(hi['hi-z_prob']==hi_test['hi-z_prob']))
                 
-    plt.scatter(Plo, Phi, edgecolor='none')
-    plt.ylim(0.8, 1.0)
-    plt.xlabel('P(lo-z)')
-    plt.ylabel('P(hi-z)')
-    plt.savefig(home_dir+'DES/magnification/lbgselect/Phi-vs-Plo')
+#    plt.scatter(Plo, Phi, edgecolor='none')
+#    plt.ylim(0.8, 1.0)
+#    plt.xlabel('P(lo-z)')
+#    plt.ylabel('P(hi-z)')
+#    plt.savefig(home_dir+'DES/magnification/lbgselect/Phi-vs-Plo')
+#    plt.close()
+
+    plt.scatter(np.arange(len(hi['hi-z_prob'])), hi['hi-z_prob']-hi_test['hi-z_prob'], edgecolor='none')
+    plt.ylabel('old code - new code')
+    plt.savefig(home_dir+'DES/magnification/lbgselect/oldhi-vs_newhi')
     plt.close()
 
 if __name__=="__main__":
