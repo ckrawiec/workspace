181c53a1d6fdf3734f06cc295b65590e19c9cf3f
Christina Krawiec <ckrawiec@sas.upenn.edu>
2016-09-12 17:58:47 -0400

    rewrite, more generic

--- zprobability.py
+++ zprobability.py
@@ -1,34 +1,36 @@
-#!/usr/bin/env python  
 """
 Calculate the probability that
 fluxes of input galaxies belong to 
 certain redshifts using photo-z's.
 
-usage: ./zprobability.py <data table> <truth table> <output>
+usage: python zprobability.py <data table> <truth table> <output>
 
 <data table> must include the following:
     -SExtractor flux and error outputs
 
 <output> will be a binary fits table
 """
-from multiprocessing import Pool
 import itertools
 import time
 import os
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
+from multiprocessing import Pool
 from astropy.io import ascii,fits
 from scipy.spatial import ckdtree
+
+usage = "usage: python zprobability.py <data table> <truth table> <output>"
 
 num_threads = 4
 
 home_dir = '/home/ckrawiec'
 this_file = '{}/git/workspace/cosmos_z-probs.py'.format(home_dir)
 
-ptype = 'full' #full or tree integration
-data_type = 'flux' #mag or flux
+ptype = 'tree' #full or tree integration
+data_type = 'FLUX' #mag or flux
 
-filters = ['g','r','i','z','Y']
+filters = ['G','R','I','Z','Y']
 
 #groups
 z_groups = [[0.2,0.8],
@@ -97,121 +99,95 @@ def ptree(vals, errs, truevals, knear=k_near):
         return np.array(out)
 
 def main(args):
+    if len(args)<4:
+        print "Missing command line argument."
+        print usage
+        sys.exit()
+
+    data_file = args[1]
+    template_file = args[2]
+    output_file = args[3]
+
     now = time.strftime("%Y-%m-%d %H:%M")
     print "#"+now
     print "num_threads="+str(num_threads)
-    print "Calculating {}-z {} probabilities, using {} integrals".format(group, data_type, ptype)
+    print "Calculating {} probabilities, using {} integrals".format(data_type, ptype)
     
     os.environ['OMP_NUM_THREADS']=str(num_threads)
 
     setup_start = time.time()
     
-    #data
-    data = fits.open(args[1])[1].data
-    truth_data = fits.open(args[2])[1].data
+    #open tables
+    data = fits.open(data_file)[1].data
+    templates = fits.open(template_file)[1].data
 
     data_time = time.time()
     print "Loaded data in {} s".format(data_time-setup_start)
 
-    #COSMOS2015 photo-z
-    #z_cosmos = 9.99 --> X-ray object, z_cosmos = 0 --> star
-    z = truth['photoz']
+    #redshifts
+    z = templates['photoz']
 
-    #cosmos fluxes and errors from sva1 gold
-    def maketable(mask=None, cosmos=False):
-        table = {}
-        for f in filters:
-            if cosmos:
-                table[f] = sva1_gold[data_type+'_detmodel_'+f][gold_cosmos15][mask]
-                table[f+'err'] = sva1_gold[data_type+'err_detmodel_'+f][gold_cosmos15][mask]
-            else:
-                table[f] = sva1_gold[data_type+'_detmodel_'+f][gold_no_cosmos]
-                table[f+'err'] = sva1_gold[data_type+'err_detmodel_'+f][gold_no_cosmos]
-        return table
+    #data vectors
+    data_zip = np.array( zip( *[data[data_type+'_detmodel_'+f] for f in filters] ) )
+    err_zip = np.array( zip( *[data[data_type+'err_detmodel_'+f] for f in filters] ) )
 
-    cosmos_tab_time = time.time()
-
-#make table from data
-#make table from truth
-
-#for each zpair
-#    mask the truth group
-#    run the code, save the Ps
-
-#when writing table, make sure to include group definitions
-#and info on column names used, input tables, nearest neighbors
-
-
-    data_cosmos = maketable(mask=group_mask, cosmos=True)
-
-    cosmos_tab_end = time.time()
-    print "Made COSMOS tables in {}s".format(cosmos_tab_end-cosmos_tab_time)
-
-    truth = np.array( zip(data_cosmos['g'],
-                          data_cosmos['r'],
-                          data_cosmos['i'],
-                          data_cosmos['z'],
-                          data_cosmos['Y']) )
-
-    sva1_tab_time = time.time()
-    data_sva1_gold = maketable()
-
-    sva1_tab_end = time.time()
-    print "Made SVA1 table in {}s".format(sva1_tab_end-sva1_tab_time)
-
-    data = np.array( zip(data_sva1_gold['g'],
-                         data_sva1_gold['r'],
-                         data_sva1_gold['i'],
-                         data_sva1_gold['z'],
-                         data_sva1_gold['Y']) )
-    data_errs = np.array( zip(data_sva1_gold['gerr'],
-                              data_sva1_gold['rerr'],
-                              data_sva1_gold['ierr'],
-                              data_sva1_gold['zerr'],
-                              data_sva1_gold['Yerr']) )
+    data_ids = data['coadd_objects_id']
+    del data
 
     setup_time = time.time()-setup_start
     print "Total setup time took {} s".format(setup_time)
-    print " "
-    print "# sva1 gold, good region mask, not in COSMOS field: {}".format(len(gold_no_cosmos))
-    print "# sva1 gold, good region mask, in COSMOS field: {}".format(len(gold_cosmos15))
-    print "# sva1 gold/COSMOS2015 matched, z>4: {}".format(len(gold_cosmos15[z4mask]))
-    print "# sva1 gold/COSMOS2015 matched, 0<z<4: {}".format(len(gold_cosmos15[z0mask & ~z4mask]))
-    print "# sva1 gold/COSMOS2015 matched, z>0: {}".format(Ntot)
-
-    start = time.time()
-
-    #multiprocessing
-    N_try = len(data)
-    print "Working on {} galaxies...".format(N_try)
     
-    pool = Pool(processes=num_threads)
+    P_dict = {}
+    N_tot = 0
 
-    n_per_process = int( np.ceil(N_try/num_threads) )
-    data_chunks = [data[i:i+n_per_process] for i in xrange(0, N_try, n_per_process)]
-    data_err_chunks = [data_errs[i:i+n_per_process] for i in xrange(0, N_try, n_per_process)]
+    for z_group in z_groups:
+        z_mask = (z >= np.min(z_group)) & (z < np.max(z_group))
+
+        template_zip = np.array( zip( *[template_data[data_type+'_detmodel_'+f][mask] for f in filters] ) )
+        N_tot += len(template_zip)
     
-    results = pool.map(pwrapper, itertools.izip(data_chunks, data_err_chunks, itertools.repeat(truth)))
+        start = time.time()
+        
+        #multiprocessing
+
+        #for testing
+        N_try = len(data_zip)
+        print "Working on {} galaxies, ...".format(N_try)
     
-    final_results = np.concatenate(results)/Ntot
+        pool = Pool(processes=num_threads)
 
-    work_time = time.time() - start
-    print "Work completed in {} s".format(work_time)
+        n_per_process = int( np.ceil(N_try/num_threads) )
+        data_chunks = [data_zip[i:i+n_per_process] for i in xrange(0, N_try, n_per_process)]
+        err_chunks = [err_zip[i:i+n_per_process] for i in xrange(0, N_try, n_per_process)]
+    
+        results = pool.map(pwrapper, itertools.izip(data_chunks, err_chunks, itertools.repeat(template_zip)))
+        
+        P_dict[str(z_group)] = np.concatenate(results)
 
+        work_time = time.time() - start
+        print "Work completed in {} s".format(work_time)
+
+    
     #write results to fits file
-    tbhdu = fits.BinTableHDU.from_columns(fits.ColDefs(
-        [fits.Column(name='coadd_objects_id', format='K', array=sva1_gold['coadd_objects_id'][gold_no_cosmos]),
-         fits.Column(name=group+'-z_prob', format='D', array=final_results)]), nrows=len(final_results))
-    prihdr = fits.Header()
-    prihdr['COMMENT'] = "Output from {}".format(this_file)
+    col_defs = [fits.Column(name='coadd_objects_id', format='K', array=data_ids)]
+
+    P_norm = np.zeros(len(data_zip))
+    for k in P_dict.keys():
+        P_norm += P_dict[k]
+    for z_group in z_groups:
+        col_defs.append(fits.Column(name='P'+str(z_group), format='D', array=P_dict[str(z_group)]/P_norm))
+
+    tb_hdu = fits.BinTableHDU.from_columns(fits.ColDefs(col_defs, nrows=len(data_ids)))
+    pri_hdr = fits.Header()
+    pri_hdr['COMMENT'] = "Bayesian redshift probabilities for data in {} using photo-zs of templates from {}. Data vectors/errors were comprised of {}(ERR)_DETMODEL_% for % in {}. Columns reported here are \'P[zmin,zmax]\'".format(data_file, template_file, ptype, filters)
     if ptype=='tree':
-        prihdr['NTREE'] = str(k_near)
-    prihdu = fits.PrimaryHDU(header=prihdr)
-    thdulist = fits.HDUList([prihdu, tbhdu])
-    thdulist.writeto(output_file)
+        pri_hdr['NTREE'] = str(k_near)
+    pri_hdu = fits.PrimaryHDU(header=pri_hdr)
+    hdu_list = fits.HDUList([pri_hdu, tb_hdu])
+    hdu_list.writeto(output_file)
     
     now = time.strftime("%Y-%m-%d %H:%M")
     print "#"+now
         
 if __name__=="__main__":
-    main()
+    main(sys.argv)
