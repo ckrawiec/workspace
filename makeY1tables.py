e5522be139f7eac356d402c6e40e5840675cad2e
Christina Krawiec <ckrawiec@sas.upenn.edu>
2016-09-19 16:46:57 -0400

    dfull table

--- makeY1tables.py
+++ makeY1tables.py
@@ -2,7 +2,7 @@ import numpy as np
 import glob
 from astropy.table import Table, vstack
 
-y1a1_gold_tables = glob.glob('/home/ckrawiec/DES/data/y1a1_gold_flux_detmodel_MC1*')
+y1a1_gold_tables = glob.glob('/home/ckrawiec/DES/data/y1a1_gold_flux_detmodel_MC1_*')
 
 tlist = []
 for table in y1a1_gold_tables:
@@ -11,6 +11,19 @@ for table in y1a1_gold_tables:
 new_Y1 = vstack(tlist)
 
 new_Y1.write('/home/ckrawiec/DES/data/y1a1_gold_flux_detmodel_MC1.fits')
+
+del tlist
+del new_Y1
+
+y1a1_gold_dfull_tables = glob.glob('/home/ckrawiec/DES/data/y1a1_gold_dfull_*')
+
+tlist = []
+for table in y1a1_gold_dfull_tables:
+    tlist.append(Table.read(tabl))
+
+new_Y1_dfull = vstack(tlist)
+
+new_Y1_dfull.write('/home/ckrawiec/DES/data/y1a1_gold_dfull.fits')
 
 #cosmos = Table.read('/home/ckrawiec/COSMOS/data/COSMOS2015_Laigle+_v1.1.fits')
 
