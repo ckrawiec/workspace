a5d6d23528e2f93e92cc09a865cde2feb1c49db3
Christina Krawiec <christina.i.krawiec@gmail.com>
2016-09-19 08:58:36 -0400

    fixed some bugs

--- makeSVtables.py
+++ makeSVtables.py
@@ -1,12 +1,12 @@
 import numpy as np
-from astropy.table import Table
+from astropy.table import Table, Column
 
 sva1_gold = Table.read('/home/ckrawiec/DES/data/sva1_gold_detmodel_gals.fits')
 cosmos = Table.read('/home/ckrawiec/COSMOS/data/COSMOS2015_Laigle+_v1.1.fits')
 
-sv_no_cosmos = np.loadtxt('/home/ckrawiec/DES/magnification/lbgselect/gold_no_cosmos_indices.txt')
-sv_cosmos = np.loadtxt('/home/ckrawiec/DES/magnification/lbgselect/gold_cosmos_indices.txt')
-cosmos_ind = np.loadtxt('/home/ckrawiec/DES/magnification/lbgselect/cosmos15_indices.txt')
+sv_no_cosmos = np.loadtxt('/home/ckrawiec/DES/magnification/lbgselect/gold_no_cosmos_indices.txt', dtype=int)
+sv_cosmos = np.loadtxt('/home/ckrawiec/DES/magnification/lbgselect/gold_cosmos15_indices.txt', dtype=int)
+cosmos_ind = np.loadtxt('/home/ckrawiec/DES/magnification/lbgselect/cosmos15_indices.txt', dtype=int)
 
 new_sv = sva1_gold[sv_no_cosmos]
 new_cosmos = sva1_gold[sv_cosmos] 
