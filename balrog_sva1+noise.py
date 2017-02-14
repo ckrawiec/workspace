08f2342a606eb77e10435a283f4e9b4b0418c128
Christina Krawiec <christina.i.krawiec@gmail.com>
2017-02-14 16:44:45 -0500

    saving before new changes

--- balrog_sva1+noise.py
+++ balrog_sva1+noise.py
@@ -2,8 +2,10 @@ import numpy as np
 from astropy.table import Table
 
 #sva1 gold with bad regions masks applied and cosmos matches removed (shouldn't matter)
-sv = Table.read('/Users/Christina/DES/data/sva1_gold_auto_good_regions_no_cosmos.fits')
-mc = (sv['MODEST_CLASS']==1)
+sv_file = '/home/ckrawiec/DES/data/sva1_gold_auto_good_regions_no_cosmos.fits'
+
+balrog_combined_output = '/home/ckrawiec/DES/data/balrog_sva1_TRUTH_zp_corr_fluxes.fits'
+balrog_noise_output = '/home/ckrawiec/DES/data/balrog_sva1_TRUTH_zp_corr_noised_fluxes.fits'
 
 #gather balrog sva1 tables
 table_nums = []
@@ -14,20 +16,46 @@ for i in range(1, 12):
 
 new_balrog = {}
 for n in table_nums:
-    b = Table.read('/Users/Christina/DES/data/balrog/sva1/balrog_sva1_auto_tab{}_SIM_TRUTH_zp_corr_fluxes.fits'.format(n))
-    for col in b.colnames:
-        if n=='01':
-            new_balrog[col] = b[col]
-            new_balrog['TABLE_INDEX'] = ['01']
-        else:
-            new_balrog[col] = np.hstack([new_balrog[col], b[col]])
-            new_balrog['TABLE_INDEX'].append(n)
+    b = Table.read('/home/ckrawiec/DES/data/balrog_sva1_auto_tab{}_SIM_TRUTH_zp_corr_fluxes.fits'.format(n))
+
+    if n=='01':
+        new_balrog['BALROG_INDEX'] = [n+'_'+str(bi) for bi in b['BALROG_INDEX']]
+        new_balrog['TABLE_INDEX'] = ['01']*len(b)
+        for band in 'GRIZ':
+            new_balrog['Z'] = b['Z']
+            new_balrog['FLUX_0_'+band] = b['FLUX_0_'+band]
+            new_balrog['FLUX_NOISED_'+band] = b['FLUX_NOISED_'+band]
+            new_balrog['FLUX_NOISELESS_'+band] = b['FLUX_NOISELESS_'+band]
+
+    else:
+        for k in new_balrog.keys():
+            if k=='TABLE_INDEX':
+                new_balrog['TABLE_INDEX'] = np.hstack([new_balrog['TABLE_INDEX'], [n]*len(b)])                
+            elif k=='BALROG_INDEX':
+                new_balrog['BALROG_INDEX'] = np.hstack([new_balrog['BALROG_INDEX'], 
+                                                       [n+'_'+str(bi) for bi in b['BALROG_INDEX']]])
+            else:
+                new_balrog[k] = np.hstack([new_balrog[k], b[k]])
+
+del b
+
+print "combined tables"
 
 #combined balrog table
-balrog = Table(new_balrog)
+balrog = Table.read(balrog_combined_output)#new_balrog)
+
+print "made new table"
 
 #write combined table
-balrog.write('/Users/Christina/DES/data/balrog/sva1/balrog_sva1_auto_SIM_TRUTH_zp_corr_fluxes.fits')
+#balrog.write(balrog_combined_output)
+
+print "written to file"
+
+#read in sv table, use galaxy-type objects
+sv = Table.read(sv_file)
+mc = (sv['MODEST_CLASS']==1)
+
+print "sv data read in"
 
 #reasonable bin cutoffs and sizes
 bins = np.linspace(0, 500, 100)
@@ -40,12 +68,15 @@ for b in 'GRIZ':
     d['flux0'] = balrog['FLUX_0_'+b]
     d['fluxNL'] = balrog['FLUX_NOISELESS_'+b]
     
+    d['0err'] = np.array([np.nan]*len(balrog))
+    d['NLerr'] = np.array([np.nan]*len(balrog))
+
     #bin the fluxes and errors
     sv_digitized = np.digitize(d['flux'], bins)
     b0_digitized = np.digitize(d['flux0'], bins)
     bNL_digitized = np.digitize(d['fluxNL'], bins)
     
-    for ibin in len(1, bins):
+    for ibin in range(1, len(bins)):
         err_median = np.median(d['err'][sv_digitized == ibin])
 
         flux0_bin = d['flux0'][b0_digitized == ibin]
@@ -57,10 +88,19 @@ for b in 'GRIZ':
         flux0_bin = flux0_bin + errs_0
         fluxNL_bin = fluxNL_bin + errs_NL
 
+        d['0err'][b0_digitized == ibin] = np.abs(errs_0)
+        d['NLerr'][bNL_digitized == ibin] = np.abs(errs_NL)
         d['flux0'][b0_digitized == ibin] = flux0_bin
         d['fluxNL'][bNL_digitized == ibin] = fluxNL_bin
 
     balrog['FLUX_0+NOISE_'+b] = d['flux0']
     balrog['FLUX_NOISELESS+NOISE_'+b] = d['fluxNL']
+    balrog['FLUXERR_0+NOISE_'+b] = d['0err']
+    balrog['FLUXERR_NOISELESS+NOISE_'+b] = d['NLerr']
+
+del sv
     
-balrog.write('/Users/Christina/DES/data/balrog/sva1/balrog_sva1_auto_SIM_TRUTH_zp_corr_noised_fluxes.fits')
+print "writing balrog+noise to file..."
+balrog.write(balrog_noise_output)
+
+print "done"
